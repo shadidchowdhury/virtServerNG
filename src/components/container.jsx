3c6396d477228271e270273f9ca99922b79678c8
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-04-14 15:15:40 +0200

    Another try

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,11 +1,39 @@
 import React, {Component} from 'react'
-var VirtServerActions = require('../actions/virtserveractions.js')
-
+import request from 'superagent'
+import jsonp from 'superagent-jsonp'
 var Container = React.createClass({
 
     handleGetVirts: function() {
        console.log('button clicked')
-       VirtServerActions.getDeployments()
+
+        //request
+        //    .post('/api/pet')
+        //    .send({ name: 'Manny', species: 'cat' })
+        //    .set('X-API-Key', 'foobar')
+        //    .set('Accept', 'application/json')
+        //    .end(function(err, res){
+        //        if (err || !res.ok) {
+        //            alert('Oh no! error');
+        //        } else {
+        //            alert('yay got ' + JSON.stringify(res.body));
+        //        }
+        //    });
+
+        request
+            .get('http://localhost:8084/login')
+            //.set('Access-Control-Allow-Origin', '*')
+            //.set('Access-Control-Allow-Headers', 'X-Requested-With')
+            .end(function(err, res){
+                console.log(res.statusCode)
+            });
+
+/*        $.ajax({
+            crossOrigin: true,
+            url: 'http://localhost:8084/login',
+            success: function(data) {
+                console.log(data);
+            }
+        });*/
     },
 
     render() {
