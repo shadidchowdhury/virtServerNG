49b826881af4a35c1c35b05a8ba53a6f8590d7a9
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-04-14 09:59:34 +0200

    Running server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (<div>
+       <h2>Hello VirtServer NG</h2>
+        </div>);
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
